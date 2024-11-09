1fef3b8c7365e42675df50f64b7e7a8911f5ce27
Arpit9026 <arpitkairati07@gmail.com>
2024-11-09 23:31:58 +0530

    user part completed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -106,13 +106,11 @@ export const updateProfile = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, bio, skills } = req.body;
         const file=req.file;
-        if (!fullname || !email || !phoneNumber || !bio || !skills) {
-            return res.status(400).json({
-                message: "Something is missing",
-                success: false
-            });
-        };
-        const skillsArray=skills.split(",");
+        let skillsArray;
+        if(skills){
+             skillsArray=skills.split(",");
+        }
+
         const userId=req.id;
         let user=await User.findById(userId);
         if(!user){
@@ -122,11 +120,11 @@ export const updateProfile = async (req, res) => {
             }); 
         };
         // Updating the data
-        user.fullname=fullname,
-        user.email=email,
-        user.phoneNumber=phoneNumber,
-        user.profile.bio=bio,
-        user.profile.skills=skillsArray
+        if(fullname) user.fullname=fullname
+        if(email) user.email=email
+        if(phoneNumber) user.phoneNumber=phoneNumber
+        if(bio) user.profile.bio=bio
+        if(skills) user.profile.skills=skillsArray
 
         await user.save();
 
