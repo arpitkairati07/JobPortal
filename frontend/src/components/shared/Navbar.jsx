030604eeb445d3485ce89fd10c6c812b5593b9d2
Arpit9026 <arpitkairati07@gmail.com>
2024-11-14 01:51:26 +0530

    ok

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
             Job <span className="text-[#F83002]">Portal</span>
           </h1>
         </div>
-        <div className='flex items-center gap-12'>
+        <div className="flex items-center gap-12">
           <ul className="flex font-medium items-center gap-5">
             <li>Home</li>
             <li>Jobs</li>
@@ -24,13 +24,16 @@ const Navbar = () => {
           </ul>
           <Popover>
             <PopoverTrigger asChild>
-              <Avatar className='cursor-pointer'>
+              <Avatar className="cursor-pointer">
                 <AvatarImage src="https://github.com/shadcn.png" />
               </Avatar>
             </PopoverTrigger>
-            <PopoverContent className='w-80'>
-              <div>
-                
+            <PopoverContent className="w-80">
+              <div className="flex gap-4 space-y-2">
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                </Avatar>
+                <h4 className="font-medium">Arpit Kairati</h4>
               </div>
             </PopoverContent>
           </Popover>
