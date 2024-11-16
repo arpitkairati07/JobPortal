c1ebde2c2b34abc7106eceff38ea8424544e0a9c
Arpit9026 <arpitkairati07@gmail.com>
2024-11-16 21:58:15 +0530

    Latest Job Cards

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
-const category=[
-    "Frontend Developer",
-    "Backend Developer",
-    "Data Scientist",
-    "Software Developer",
-    "Full Stack Developer",
-    "Machine Learning Engineer",
-    "Cloud Enginner",
-    "Network Engineer",
-    "DataBase Administrator"
-]
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import { Button } from "./ui/button";
+
+const category = [
+  "Frontend Developer",
+  "Backend Developer",
+  "Data Scientist",
+  "Software Developer",
+  "Full Stack Developer",
+  "Machine Learning Engineer",
+  "Cloud Engineer",
+  "Network Engineer",
+  "Database Administrator",
+];
 
 const CategoryCarousel = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <Carousel className="w-full max-w-xl mx-auto my-20">
+        <CarouselContent>
+          {category.map((cat, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg-basis-1/3">
+              <Button variant="outline" className="rounded-full">
+                {cat}
+              </Button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="bg-gray-200 p-2 rounded-full shadow-md cursor-pointer transition-transform duration-200 hover:bg-gray-300 hover:scale-105">
+          <span>&larr;</span>
+        </CarouselPrevious>
 
-export default CategoryCarousel
+        <CarouselNext className="bg-gray-200 p-2 rounded-full shadow-md cursor-pointer transition-transform duration-200 hover:bg-gray-300 hover:scale-105">
+          <span>&rarr;</span>
+        </CarouselNext>
+      </Carousel>
+    </div>
+  );
+};
+
+export default CategoryCarousel;
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './shared/Navbar'
 import Hero from './Hero'
 import CategoryCarousel from './CategoryCarousel'
+import LatestJobs from './LatestJobs'
 
 const Home = () => {
   return (
@@ -9,8 +10,8 @@ const Home = () => {
       <Navbar></Navbar>
       <Hero></Hero>
        <CategoryCarousel></CategoryCarousel>
-      {/* <LatestJobs></LatestJobs>
-      <Footer></Footer>  */}
+      <LatestJobs></LatestJobs>
+      {/* <Footer></Footer>  */}
     </div>
   )
 }
