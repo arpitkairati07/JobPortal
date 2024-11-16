6a05112c7cbfb4440cb1016821a231a633697390
Arpit9026 <arpitkairati07@gmail.com>
2024-11-16 17:12:48 +0530

    category

---
+++ frontend/src/components/CategoryCarousel.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+const category=[
+    "Frontend Developer",
+    "Backend Developer",
+    "Data Scientist",
+    "Software Developer",
+    "Full Stack Developer",
+    "Machine Learning Engineer",
+    "Cloud Enginner",
+    "Network Engineer",
+    "DataBase Administrator"
+]
+
+const CategoryCarousel = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CategoryCarousel
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
 import Hero from './Hero'
+import CategoryCarousel from './CategoryCarousel'
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
-      {/* <CategoryCarousel></CategoryCarousel>
-      <LatestJobs></LatestJobs>
-      <Footer></Footer> */}
+       <CategoryCarousel></CategoryCarousel>
+      {/* <LatestJobs></LatestJobs>
+      <Footer></Footer>  */}
     </div>
   )
 }
